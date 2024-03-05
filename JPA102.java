2ebc759a4b6ebe263ffa479d94daaaa8261f0bc3
112B16727 <162256020+112B16727@users.noreply.github.com>
2024-03-05 10:26:02 +0800

    Add files via upload

---
+++ JPA102.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class JPA102 {
+	public static void main (String[] args) {
+		System.out.println("Please input:");
+		double k = new Scanner(System.in).nextDouble();
+		System.out.println(k+" kg = "+ (k*2.20462) + " ponds");
+	}
+}
---
+++ JPA103.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class JPA103 {
+	public static void main (String[] args) {
+		System.out.println("Please input:");
+		Scanner sc = new Scanner(System.in);
+		int a,b,c;
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=sc.nextInt();
+		System.out.printf("Average: %4.2f",((double)(a+b+c)/3));
+	}
+}
